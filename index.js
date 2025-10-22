d6d2c9e1a141c1d9ec57309e1ea796c65d99ff44
LexiD5247 <deakinslexi@gmail.com>
2025-10-22 09:49:12 -0500

    adding email js

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 //template_4gsd1xf
 //service_05ic6o7
 //OILUGn6Bsuv4ITP-D
+
+function contact(event) {
+    event.preventDefault();
+    const loading = document.querySelector('.modal__overlay--loading');
+    const success = document.querySelector('.modal__overlay--success');
+    loading.classList += " modal__overlay--visible"
+    emailjs
+    .sendForm(
+       'service_05ic6o7',
+       'template_4gsd1xf',
+       event.target,
+       'OILUGn6Bsuv4ITP-D'
+    ).then(() => {
+    loading.classList.remove("modal__overlay--visible");
+    success.classList += " modal__overlay--visible";
+    }).catch(() => {
+        loading.classList.remove("modal__overlay--visible")
+        alert(
+            "The email service if temporarily unavaliable. Please email me directly at email@email.com"
+        );
+    })
+}
